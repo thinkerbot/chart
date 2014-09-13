b91b0178264facfa9ac18917c66792bc3acd76ec
Simon Chiang <schiang@comverge.com>
2014-09-13 04:33:22 -0600

    Generate and show configs when url is set

--- lib/chart/receivers/line.rb
+++ lib/chart/receivers/line.rb
@@ -2,7 +2,12 @@ module Chart
   module Receivers
     class Line
       def call(nxyz)
-        nxyz ? [nxyz] : nil
+        if nxyz
+          n, x, y, z = nxyz
+          [[n, {"dimensions" => ["i", "i", "i"]}, [[x,y,z]]]]
+        else
+          nil
+        end
       end
     end
   end
--- lib/chart/senders/echo.rb
+++ lib/chart/senders/echo.rb
@@ -1,12 +1,14 @@
 require 'chart/sender'
+require 'json'
 
 module Chart
   module Senders
     class Echo < Sender
-      def call(data)
-        data.each do |nxyz|
-          fields = nxyz.join(' ')
-          puts(url ? File.join(url, fields) : fields)
+      def call(datasets)
+        datasets.each do |topic, config, data|
+          # post topic with configs and data -- blows up if configs are different and not force
+          format = url ? "#{File.join(url, topic)} %s %s %s #{config.to_json}" : "#{topic} %s %s %s"
+          data.each {|xyz| puts format % xyz }
         end
       end
     end
