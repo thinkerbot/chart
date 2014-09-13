fe632af6b7d658b132f3334bafe57c42e7233f74
Simon Chiang <schiang@comverge.com>
2014-09-13 05:00:29 -0600

    Sketch out how data dimensions could be guessed
    This particular code is not long for the world.  It is better to do this work
    server-side than client-side.

--- lib/chart/receivers/line.rb
+++ lib/chart/receivers/line.rb
@@ -1,10 +1,39 @@
 module Chart
   module Receivers
     class Line
+      def initialize
+        @configs = {}
+        @cache = {}
+      end
+
+      def config_for(topic)
+        @configs[topic] ||= guess_config_for(topic)
+      end
+
+      def guess_config_for(topic)
+        data = data_for(topic)
+        dimensions = data.first.map do |field|
+          case field
+          when /^\d+$/ then "i"
+          when /^\d+\.\d+$/ then "d"
+          when /^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d(?:\.\d+)?(?:Z|[+-]\d\d:\d\d)$/ then "t"
+          else "s"
+          end
+        end
+        {"dimensions" => dimensions}
+      end
+
+      def data_for(topic, new_data = nil)
+        current = @cache[topic] ||= []
+        @cache[topic] = new_data if new_data
+        current
+      end
+
       def call(nxyz)
         if nxyz
-          n, x, y, z = nxyz
-          [[n, {"dimensions" => ["i", "i", "i"]}, [[x,y,z]]]]
+          topic, x, y, z = nxyz
+          data_for(topic) << [x,y,z]
+          [[topic, config_for(topic), data_for(topic, [])]]
         else
           nil
         end
