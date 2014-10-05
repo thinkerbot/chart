c0982034038b3b6fa7ccb5c33509fb02b5e2e420
Simon Chiang <simon.a.chiang@gmail.com>
2014-10-05 12:44:36 -0600

    Remove error implementation of unused VarcharColumn methods

--- lib/chart/columns/varchar_column.rb
+++ lib/chart/columns/varchar_column.rb
@@ -24,14 +24,6 @@ module Chart
       def serialize(value)
         value
       end
-
-      def offset(value, period_str)
-        raise "offsets aren't a thing for varchar type"
-      end
-
-      def pkey(value)
-        raise "using varchar as x isn't yet a thing"
-      end
     end
   end
 end
